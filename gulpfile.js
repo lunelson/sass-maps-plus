1c314dbadb574e6b69e73ae34a705eb4058857db
Lu Nelson <lunelson@gmail.com>
2015-01-05 12:26:21 +0100

    update package and gulpfile, tests working

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
-//////////////////
-// definitions //
-//////////////////
-
+var autoprefixer   = require('gulp-autoprefixer');
+var buffer         = require('vinyl-buffer');
+var fs             = require('fs');
 var gulp           = require('gulp');
-var sass           = require('gulp-sass');
+var gutil          = require('gulp-util');
+var plumber        = require('gulp-plumber');
+var rename         = require("gulp-rename");
+var run            = require('gulp-run');
 var strip_comments = require('gulp-strip-json-comments');
 
-//////////
-// sass //
-//////////
-
-// task: sass
-gulp.task('sass', function () {
-    gulp.src('test/test.scss')
-        .pipe(sass({
-            errLogToConsole: false,
-            onError: function(err) {
-                console.log(err);
-            }
-        }))
-        // .pipe(strip_comments())
-        .pipe(gulp.dest('test'));
+gulp.task('sassc', function () {
+  gulp.src('test/test.scss', { buffer: false })
+    .pipe(plumber(function(err) {
+        gutil.beep();
+        var errorTxt = err.message +'\n\n'+ err.source;
+        gutil.log(gutil.colors.red(errorTxt));
+        // fs.writeFile('test.log', errorTxt);
+    }))
+    .pipe(run('/applications/libsass/sassc/bin/sassc -s', {verbosity: 1}))
+    .pipe(rename(function (path) { path.extname = ".css"; }))
+    .pipe(buffer())
+    // .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    // .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('test/'))
 });
 
 ///////////
@@ -28,7 +30,7 @@ gulp.task('sass', function () {
 ///////////
 
 gulp.task('watch', function () {
-    gulp.watch('test/test.scss', ['sass']);
+  gulp.watch('test/test.scss', ['sassc']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sassc', 'watch']);
