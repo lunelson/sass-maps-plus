01807280892481ac35a35eeafcca9e852c94400c
Lu Nelson <lunelson@gmail.com>
2016-10-08 11:08:10 +0200

    clean up test directory

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
 var path = require('path');
 var sassTrue = require('sass-true');
-var sassFile = path.join(__dirname, '_test.scss');
+var sassFile = path.join(__dirname, '_index.scss');
 sassTrue.runSass({file: sassFile}, describe, it);
